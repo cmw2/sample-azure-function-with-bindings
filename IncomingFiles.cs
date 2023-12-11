d6ce98fbdcd1481e4e7d4aaabd4a7c7879dbdfa2
Chris Westbrook <westbrook.chris@gmail.com>
2023-12-11 15:45:40 -0500

    initial commit with working blob binding and trigger

---
+++ IncomingFiles.cs
@@ -0,0 +1,25 @@
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace pdo_appearance
+{
+    public class IncomingFiles
+    {
+        private readonly ILogger<IncomingFiles> _logger;
+
+        public IncomingFiles(ILogger<IncomingFiles> logger)
+        {
+            _logger = logger;
+        }
+
+        [Function(nameof(IncomingFiles))]
+        public async Task Run([BlobTrigger("incoming-local/{name}", Connection = "storcascappinspoc_STORAGE")] Stream stream, string name)
+        { 
+            using var blobStreamReader = new StreamReader(stream);
+            var content = await blobStreamReader.ReadToEndAsync();
+            _logger.LogInformation($"C# Blob trigger function Processed blob\n Name: {name} \n Data: {content}");
+        }
+    }
+}
