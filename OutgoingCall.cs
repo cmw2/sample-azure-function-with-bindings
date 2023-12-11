5c1cc9df2f956ae3c68c8bea7fe8e68f7cadf657
Chris Westbrook <westbrook.chris@gmail.com>
2023-12-11 16:37:20 -0500

    added function to read SB and call API

---
+++ OutgoingCall.cs
@@ -0,0 +1,34 @@
+using System;
+using Azure.Messaging.ServiceBus;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace pdo_appearance
+{
+    public class OutgoingCall
+    {
+        private readonly ILogger<OutgoingCall> _logger;
+        private readonly IHttpClientFactory _httpClientFactory;
+        private readonly IConfiguration _configuration;
+
+        public OutgoingCall(ILogger<OutgoingCall> logger, IHttpClientFactory httpClientFactory, IConfiguration configuration)
+        {
+            _logger = logger;
+            _httpClientFactory = httpClientFactory;
+            _configuration = configuration;
+        }
+
+        [Function(nameof(OutgoingCall))]
+        public async Task Run([ServiceBusTrigger("outputQueue", Connection = "SBCon")] ServiceBusReceivedMessage message)
+        {
+            _logger.LogInformation("Message ID: {id}", message.MessageId);
+            _logger.LogInformation("Message Body: {body}", message.Body);
+            _logger.LogInformation("Message Content-Type: {contentType}", message.ContentType);
+            var client = _httpClientFactory.CreateClient();
+            var content = new ByteArrayContent(message.Body.ToArray()); 
+            content.Headers.Add("Ocp-Apim-Subscription-Key",_configuration["TargetKey"]);           
+            await client.PostAsync("https://apim-casc-appins-vbd.azure-api.net/target/service", content);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,7 +1,13 @@
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 var host = new HostBuilder()
     .ConfigureFunctionsWorkerDefaults()
+    .ConfigureServices(services =>
+    {
+        services.AddHttpClient();
+    })
     .Build();
+
 
 host.Run();
